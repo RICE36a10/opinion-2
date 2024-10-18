2adbc83b08b06d98636ce3dbc355fddc7c5d93fd
RICE36a10 <111008452+RICE36a10@users.noreply.github.com>
2024-10-18 16:02:45 +0530

    Update public submodule

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAnZoU0S9VcQNqtlP0Eo-oFfYD9Q2jg1RE",
-  authDomain: "original-opinion-1.firebaseapp.com",
-  projectId: "original-opinion-1",
-  storageBucket: "original-opinion-1.appspot.com",
-  messagingSenderId: "439916396436",
-  appId: "1:439916396436:web:df16ede09e526540af5981",
-  measurementId: "G-N4PGR1V4GM"
+  apiKey: "AIzaSyCrXyShvgRR8bV-HZY1XRWFC4yS6--sLkc",
+  authDomain: "opinion2-5ff6e.firebaseapp.com",
+  projectId: "opinion2-5ff6e",
+  storageBucket: "opinion2-5ff6e.appspot.com",
+  messagingSenderId: "848059314753",
+  appId: "1:848059314753:web:e9887a670598e2042f0cc0"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
